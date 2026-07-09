b0577c6fb3271c0f53fc71ca9c77da48e4b0a39e
Anna Kim <55574696+kimanna10@users.noreply.github.com>
2026-07-10 02:20:43 +0900

    Add revalidation for contacts page

--- front-back/src/app/contacts/page.js
+++ front-back/src/app/contacts/page.js
@@ -3,6 +3,8 @@ import ContactSection from "@/components/sections/ContactSection";
 export const metadata = {
   title: "CONTACTS | RAKHMETULAYEVA KAMILA",
 };
+export const revalidate = 10;
+
 export default function Contact() {
   return (
     <Section title="Contacts" id="contact">
